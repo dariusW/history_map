dbb0b598069b683d3b18998f06a3d3a427c10ec2
dariusW <dariusz.wolek@gmail.com>
2012-09-02 16:45:53 +0200

    Initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HistoryMap::Application.config.secret_token = '0221cea642f7d5fec2c9a913bab7dcaee42fe7147f1c901eb75833ae5dac2010bd8531a630debfb913876af069dfb619e3bf4d598f587710beeb215b8c2b8b9a'
